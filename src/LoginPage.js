f4993087907ea05bc0db58668326c1d629a38536
taniayunita <taniayunita22@gmail.com>
2021-09-09 11:13:30 +0700

    membuat login sederhana

---
+++ src/LoginPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './LoginPage.css'
+
+const LoginPage = () => {
+    return (
+        <div className='container'>
+            <input/>
+            <input/>
+            <button>Log in</button>
+        </div>
+    )
+}
+
+export default LoginPage
+
